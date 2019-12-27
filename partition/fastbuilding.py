19810335ab7475a649fdff11e9f9850d5af7f6a4
ralzq01 <ralzq01@163.com>
2019-12-27 12:18:59 +0800

    partition bug fixed. enable parallel

--- partition/fastbuilding.py
+++ partition/fastbuilding.py
@@ -15,6 +15,7 @@ import numpy as np
 import scipy.sparse as spsp
 import argparse
 import PaGraph.data as data
+import torch.multiprocessing as mp
 
 
 def get_sub_graph(dgl_g, train_nid, num_hops):
@@ -31,33 +32,55 @@ def get_sub_graph(dgl_g, train_nid, num_hops):
   
   assert(len(nfs) == 1)
   nf = nfs[0]
-  full_edge_src = []
-  full_edge_dst = []
-  for i in range(nf.num_blocks):
-    nf_src_nids, nf_dst_nids, _ = nf.block_edges(i, remap_local=False)
-    full_edge_src.append(nf.map_to_parent_nid(nf_src_nids))
-    full_edge_dst.append(nf.map_to_parent_nid(nf_dst_nids))
-  full_srcs = torch.cat(tuple(full_edge_src)).numpy()
-  full_dsts = torch.cat(tuple(full_edge_dst)).numpy()
-  # set up mappings
-  sub2full = np.unique(np.concatenate((full_srcs, full_dsts)))
-  full2sub = np.zeros(np.max(sub2full) + 1, dtype=np.int64)
-  full2sub[sub2full] = np.arange(len(sub2full), dtype=np.int64)
-  # map to sub graph space
-  sub_srcs = full2sub[full_srcs]
-  sub_dsts = full2sub[full_dsts]
+  
+  full_eids = nf._edge_mapping
+  subg = dgl_g.edge_subgraph(full_eids, preserve_nodes=False)
+  sub2full = subg._parent_nid.tonumpy()
+  tmax = np.max(sub2full)
+  tmin = np.min(sub2full)
+  train_nid = np.where(train_nid <= tmax, train_nid, tmin)
+  subtrainid = subg.map_to_subgraph_nid(np.unique(train_nid))
+  coo_adj = subg.adjacency_matrix_scipy(transpose=True, fmt='coo', return_edge_ids=False)
   vnum = len(sub2full)
-  enum = len(sub_dsts)
+  enum = coo_adj.row.shape[0]
   print('vertex#: {} edge#: {}'.format(vnum, enum))
-  data = np.ones(sub_srcs.shape[0], dtype=np.uint8)
-  coo_adj = spsp.coo_matrix((data, (sub_srcs, sub_dsts)), shape=(vnum, vnum))
-  # train nid
-  tnid = nf.layer_parent_nid(-1).numpy()
-  valid_t_max = np.max(sub2full)
-  valid_t_min = np.min(tnid)
-  tnid = np.where(tnid <= valid_t_max, tnid, valid_t_min)
-  subtrainid = full2sub[np.unique(tnid)]
   return coo_adj, sub2full, subtrainid
+
+
+def worker_partition(rank, args, dgl_g, train_nid):
+  labels = data.get_labels(args.dataset)
+  partition_dataset = os.path.join(args.dataset, '{}naive'.format(args.partition))
+  try:
+    os.mkdir(partition_dataset)
+  except FileExistsError:
+    pass
+  # get train vertices partitions
+  chunk_size = int(len(train_nid) / args.partition)
+  start_ofst = chunk_size * rank
+  if rank == args.partition - 1:
+    end_ofst = len(train_nid)
+  else:
+    end_ofst = start_ofst + chunk_size
+  part_nid = train_nid[start_ofst:end_ofst]
+  subadj, sub2fullid, subtrainid = get_sub_graph(dgl_g, part_nid, args.num_hops)
+  sublabel = labels[sub2fullid[subtrainid]]
+  # files
+  subadj_file = os.path.join(
+    partition_dataset,
+    'subadj_{}.npz'.format(str(rank)))
+  sub_trainid_file = os.path.join(
+    partition_dataset,
+    'sub_trainid_{}.npy'.format(str(rank)))
+  sub_train2full_file = os.path.join(
+    partition_dataset,
+    'sub_train2fullid_{}.npy'.format(str(rank)))
+  sub_label_file = os.path.join(
+    partition_dataset,
+    'sub_label_{}.npy'.format(str(rank)))
+  spsp.save_npz(subadj_file, subadj)
+  np.save(sub_trainid_file, subtrainid)
+  np.save(sub_train2full_file, sub2fullid)
+  np.save(sub_label_file, sublabel)
 
 
 if __name__ == "__main__":
@@ -76,44 +99,7 @@ if __name__ == "__main__":
   train_mask, val_mask, test_mask = data.get_masks(args.dataset)
   train_nid = np.nonzero(train_mask)[0].astype(np.int64)
   np.random.shuffle(train_nid)
-  labels = data.get_labels(args.dataset)
 
-  # save
-  adj_file = os.path.join(args.dataset, 'adj.npz')
-  mask_file = os.path.join(args.dataset, 'train.npy')
-  label_file = os.path.join(args.dataset, 'labels.npy')
-  partition_dataset = os.path.join(args.dataset, '{}naive'.format(args.partition))
-  try:
-    os.mkdir(partition_dataset)
-  except FileExistsError:
-    pass
-
-  chunk_size = int(len(train_nid) / args.partition) + 1
-  for pid in range(args.partition):
-    start_ofst = chunk_size * pid
-    if pid == args.partition - 1:
-      end_ofst = len(train_nid)
-    else:
-      end_ofst = start_ofst + chunk_size
-    part_nid = train_nid[start_ofst:end_ofst]
-    subadj, sub2fullid, subtrainid = get_sub_graph(dgl_g, part_nid, args.num_hops)
-    sublabel = labels[part_nid]
-    # files
-    subadj_file = os.path.join(
-      partition_dataset,
-      'subadj_{}.npz'.format(str(pid)))
-    sub_trainid_file = os.path.join(
-      partition_dataset,
-      'sub_trainid_{}.npy'.format(str(pid)))
-    sub_train2full_file = os.path.join(
-      partition_dataset,
-      'sub_train2fullid_{}.npy'.format(str(pid)))
-    sub_label_file = os.path.join(
-      partition_dataset,
-      'sub_label_{}.npy'.format(str(pid)))
-    spsp.save_npz(subadj_file, subadj)
-    np.save(sub_trainid_file, subtrainid)
-    np.save(sub_train2full_file, sub2fullid)
-    np.save(sub_label_file, sublabel)
+  mp.spawn(worker_partition, args=(args, dgl_g, train_nid), nprocs=args.partition, join=True)
   
   
