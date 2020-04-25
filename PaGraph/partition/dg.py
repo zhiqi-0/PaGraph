dcca40e9b1e642fb82ea432a379e4c8fdd8ebdd8
ralzq01 <ralzq01@163.com>
2020-04-25 22:19:54 +0800

    add graph ordering

--- PaGraph/partition/dg.py
+++ PaGraph/partition/dg.py
@@ -1,13 +1,5 @@
 import os
 import sys
-# set environment
-module_name ='PaGraph'
-modpath = os.path.abspath('.')
-if module_name in modpath:
-  idx = modpath.find(module_name)
-  modpath = modpath[:idx]
-sys.path.append(modpath)
-
 import dgl
 from dgl import DGLGraph
 import torch
@@ -15,11 +7,21 @@ import numpy as np
 import scipy.sparse as spsp
 import argparse
 import PaGraph.data as data
-import random
+import ordering
 
 
 def in_neighbors(csc_adj, nid):
   return csc_adj.indices[csc_adj.indptr[nid]: csc_adj.indptr[nid+1]]
+
+
+def in_neighbors_hop(csc_adj, nid, hops):
+  if hops == 1:
+    return in_neighbors(csc_adj, nid)
+  else:
+    nids = []
+    for depth in range(hops):
+      nids.append(in_neighbors(csc_adj, nid))
+    return np.unique(np.hstack(nids))
 
 
 def dg_max_score(score, p_vnum):
@@ -51,46 +53,62 @@ def dg_ind(adj, neighbors, belongs, p_vnum, r_vnum, pnum):
   return score
 
 
-def dg(args, adj, train_nids):
+def dg(partition_num, adj, train_nids):
   csc_adj = adj.tocsc()
   vnum = adj.shape[0]
+  vtrain_num = train_nids.shape[0]
   belongs = -np.ones(vnum, dtype=np.int8)
-  r_belongs = [-np.ones(vnum, dtype=np.int8) for _ in range(args.partition)]
-  p_vnum = np.zeros(args.partition, dtype=np.int64)
-  r_vnum = np.zeros(args.partition, dtype=np.int64)
+  r_belongs = [-np.ones(vnum, dtype=np.int8) for _ in range(partition_num)]
+  p_vnum = np.zeros(partition_num, dtype=np.int64)
+  r_vnum = np.zeros(partition_num, dtype=np.int64)
 
   progress = 0
   #for nid in range(0, train_nids):
-  print(train_nids.shape[0])
-  for nid in train_nids:  
+  print('total vertices: {} | train vertices: {}'.format(vnum, vtrain_num))
+  for step, nid in enumerate(train_nids):  
     neighbors = in_neighbors(csc_adj, nid)
-    score = dg_ind(csc_adj, neighbors, belongs, p_vnum, r_vnum, args.partition)
+    score = dg_ind(csc_adj, neighbors, belongs, p_vnum, r_vnum, partition_num)
     ind = dg_max_score(score, p_vnum)
     if belongs[nid] == -1:
       belongs[nid] = ind
       p_vnum[ind] += 1
-      #random_num = random.random()
-      #if random_num < 0.65:
       neighbors = np.append(neighbors, nid)
       for neigh_nid in neighbors:
         if r_belongs[ind][neigh_nid] == -1:
           r_belongs[ind][neigh_nid] = 1
           r_vnum[ind] += 1
     # progress
-    if int(vnum * progress / 100) <= nid:
-      sys.stdout.write(' {}%\r'.format(progress))
+    if int(vtrain_num * progress / 100) <= step:
+      sys.stdout.write('=>{}%\r'.format(progress))
       sys.stdout.flush()
       progress += 1
+  print('')
   
-  for pid in range(args.partition):
+  sub_v = []
+  sub_trainv = []
+  for pid in range(partition_num):
     p_trainids = np.where(belongs == pid)[0]
-    np.save('{}trainids'.format(pid), p_trainids)
-    print('redundancy vertex#  ', r_vnum[pid])
-    print('original vertex# ', p_vnum[pid])
+    sub_trainv.append(p_trainids)
+    p_v = np.where(r_belongs[pid] != -1)[0]
+    sub_v.append(p_v)
+    assert p_v.shape[0] == r_vnum[pid]
+    print('vertex# with self-reliance: ', r_vnum[pid])
+    print('vertex# w/o  self-reliance: ', p_vnum[pid])
     #print('orginal vertex: ', np.where(belongs == pid)[0])
     #print('redundancy vertex: ', np.where(r_belongs[pid] != -1)[0])
+  return sub_v, sub_trainv
 
-  
+
+def node2graph(fulladj, nodelist, train_nids):
+  g = dgl.DGLGraph(fulladj)
+  subg = g.subgraph(nodelist)
+  sub2full = subg.parent_nid.numpy()
+  subadj = subg.adjacency_matrix_scipy(transpose=True, return_edge_ids=False)
+  # get train vertices under subgraph scope
+  subtrain = subg.map_to_subgraph_nid(train_nids).numpy()
+  return subadj, sub2full, subtrain
+
+
 
 
 if __name__ == '__main__':
@@ -101,9 +119,44 @@ if __name__ == '__main__':
                       help="num of partitions")
   parser.add_argument("--num-hop", type=int, default=1,
                       help="num of hop neighbors required for a batch")
+  parser.add_argument("--ordering", dest='ordering', action='store_true')
+  parser.set_defaults(ordering=False)
   args = parser.parse_args()
 
+  # get data
   adj = spsp.load_npz(os.path.join(args.dataset, 'adj.npz'))
   train_mask, val_mask, test_mask = data.get_masks(args.dataset)
   train_nids = np.nonzero(train_mask)[0].astype(np.int64)
-  dg(args, adj, train_nids)
+  labels = data.get_labels(args.dataset)
+  # ordering
+  if args.ordering:
+    print('re-ordering graphs...')
+    adj = adj.tocsc()
+    adj, vmap = ordering.reordering(adj, depth=args.num_hop) # vmap: orig -> new
+    train_nids = np.sort(vmap[train_nids])
+  # partition
+  p_v, p_trainv = dg(args.partition, adj, train_nids)
+  
+  # save to file
+  partition_dataset = os.path.join(args.dataset, '{}naive'.format(args.partition))
+  for pid, (pv, ptrainv) in enumerate(zip(p_v, p_trainv)):
+    print('genering subgraph# {}'.format(pid))
+    subadj, sub2fullid, subtrainid = node2graph(adj, pv, ptrainv)
+    sublabel = labels[sub2fullid[subtrainid]]
+    # files
+    subadj_file = os.path.join(
+      partition_dataset,
+      'subadj_{}.npz'.format(str(pid)))
+    sub_trainid_file = os.path.join(
+      partition_dataset,
+      'sub_trainid_{}.npy'.format(str(pid)))
+    sub_train2full_file = os.path.join(
+      partition_dataset,
+      'sub_train2fullid_{}.npy'.format(str(pid)))
+    sub_label_file = os.path.join(
+      partition_dataset,
+      'sub_label_{}.npy'.format(str(pid)))
+    spsp.save_npz(subadj_file, subadj)
+    np.save(sub_trainid_file, subtrainid)
+    np.save(sub_train2full_file, sub2fullid)
+    np.save(sub_label_file, sublabel)
