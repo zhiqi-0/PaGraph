df3cc93916e9b7f662a014a969e6d95ca902b7f9
ralzq01 <ralzq01@163.com>
2020-05-05 08:51:18 +0800

    fix small bugs

--- examples/eval.py
+++ examples/eval.py
@@ -1,13 +1,5 @@
 import os
 import sys
-# set environment
-module_name ='PaGraph'
-modpath = os.path.abspath('.')
-if module_name in modpath:
-  idx = modpath.find(module_name)
-  modpath = modpath[:idx]
-sys.path.append(modpath)
-
 import argparse, time
 import torch
 import torch.nn as nn
@@ -18,14 +10,11 @@ import numpy as np
 import dgl
 import PaGraph.data as data
 
-def gnneval(args, infer_model, train_model, graph, labels, rank, test_nid):
+def gnneval(args, infer_model, graph, labels, rank, test_nid):
   """
   Evaluation just on a single machine
   """
   ctx = torch.device(rank)
-  for infer_param, param in zip(infer_model.parameters(), train_model.parameters()):    
-    infer_param.data.copy_(param.data)
-  infer_model.cuda(ctx)
 
   num_hops = args.n_layers if args.preprocess else args.n_layers + 1
   for nf in dgl.contrib.sampling.NeighborSampler(graph,len(test_nid),
@@ -36,7 +25,8 @@ def gnneval(args, infer_model, train_model, graph, labels, rank, test_nid):
                                                  seed_nodes=test_nid):
     nf.copy_from_parent(ctx=ctx)
   
-  for ckpt in range(5,60,5):
+  for ckpt in range(args.start,args.end,args.interval):
+    nf.copy_from_parent(ctx=ctx)
     train_model = torch.load(
       os.path.join(args.ckpt, args.arch + '_' + str(ckpt))
     )
@@ -53,7 +43,7 @@ def gnneval(args, infer_model, train_model, graph, labels, rank, test_nid):
       batch_labels = labels[batch_nids].cuda(rank)
       num_acc += (pred.argmax(dim=1) == batch_labels).sum().cpu().item()
     
-    print("Test Accuracy {:.4f}".format(num_acc / len(test_nid)))
+    print("[{}]: Test Accuracy {:.4f}".format(ckpt, num_acc / len(test_nid)))
 
 
 def main(args):
@@ -68,23 +58,17 @@ def main(args):
   labels = torch.LongTensor(labels)
 
   if args.arch == 'gcn-nssc':
-    from PaGraph.model.pytorch.gcn_nssc import GCNSampling, GCNInfer
-    train_model = torch.load(
-      os.path.join(args.ckpt, args.arch + '_' + str(args.epoch))
-    )
+    from PaGraph.model.gcn_nssc import GCNSampling, GCNInfer
     infer_model = GCNInfer(args.feat_size,
-                           args.n_hidden,
+                           32,
                            n_classes,
                            args.n_layers,
                            F.relu,
                            args.preprocess)
   elif args.arch == 'gs-nssc':
-    from PaGraph.model.pytorch.graphsage_nssc import GraphSageSampling
-    train_model = torch.load(
-      os.path.join(args.ckpt, args.arch + '_' + str(args.epoch))
-    )
+    from PaGraph.model.graphsage_nssc import GraphSageSampling
     infer_model = GraphSageSampling(args.feat_size,
-                            args.n_hidden,
+                            16,
                             n_classes,
                             args.n_layers,
                             F.relu,
@@ -95,7 +79,7 @@ def main(args):
     print('Unknown arch')
     sys.exit(-1)
   
-  gnneval(args, infer_model, train_model, g, labels, 0, test_nid)
+  gnneval(args, infer_model, g, labels, 0, test_nid)
 
 
 if __name__ == '__main__':
@@ -110,15 +94,15 @@ if __name__ == '__main__':
   # model arch
   parser.add_argument("--feat-size", type=int, default=602,
                       help='input feature size')
-  parser.add_argument("--n-hidden", type=int, default=32,
-                      help="number of hidden gcn units")
   parser.add_argument("--n-layers", type=int, default=1,
                       help="number of hidden gcn layers")
   # training hyper-params
-  parser.add_argument("--epoch", type=int, default=60,
-                      help="eval epcoh")
-  parser.add_argument("--batch-size", type=int, default=512,
-                      help="batch size")
+  parser.add_argument("--start", type=int, default=0,
+                      help="eval epoch start")
+  parser.add_argument("--interval", type=int, default=5,
+                      help="eval epoch interval")
+  parser.add_argument("--end", type=int, default=60,
+                      help='eval epoch end (not include)')
   
   parser.add_argument("--ckpt", type=str, default='checkpoint',
                       help="checkpoint dir")
